5ae1260ce9b604835ac6e9052248b77f3e3560df
Kahthegreat <sirkahtenn@gmail.com>
2014-10-04 14:53:21 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
-/**
- * 
- */
+(function() {
+  var app = angular.module('Quickmune', []);
+
+  app.controller('', function(){
+    this.product = vaccine;
+  });
+
+  var vaccines = {
+    name: 'Tetanus',
+    description: 'Tetanus vaccine is a vaccine composed of deactivated tetanus toxins. This vaccine is	immunogenic but not pathogenic and is used to prevent an individual from contracting tetanus.', 
+    isAvailable: true ,
+    location: 'Worldwide'
+  };
+  })();
